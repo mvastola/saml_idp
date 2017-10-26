00dfc68541723fff4a10e7366dfe68bf0f950d70
Andy Brody <andrew.brody@gsa.gov>
2017-10-26 17:47:23 -0400

    Add tests for the name_id_format functionality.
    Check that the AssertionBuilder correctly generates assertions with the
    requested NameID format when the name_id_format method is present on the
    principal.

--- spec/lib/saml_idp/assertion_builder_spec.rb
+++ spec/lib/saml_idp/assertion_builder_spec.rb
@@ -76,6 +76,99 @@ module SamlIdp
       end
     end
 
+    describe 'with principal.name_id_format' do
+      let(:config) { SamlIdp::Configurator.new }
+      before do
+        config.name_id.formats = {
+          persistent: ->(p) { 'my-example-id' },
+          email_address: ->(p) { 'foo@example.com' },
+        }
+        allow(SamlIdp).to receive_messages(config: config)
+      end
+
+      it "uses the first name ID format if none is specified" do
+        Timecop.travel(Time.zone.local(2010, 6, 1, 13, 0, 0)) do
+          expect(subject.raw).to eq("<Assertion xmlns=\"urn:oasis:names:tc:SAML:2.0:assertion\" ID=\"_abc\" IssueInstant=\"2010-06-01T13:00:00Z\" Version=\"2.0\"><Issuer>http://sportngin.com</Issuer><Subject><NameID Format=\"urn:oasis:names:tc:SAML:2.0:nameid-format:persistent\">my-example-id</NameID><SubjectConfirmation Method=\"urn:oasis:names:tc:SAML:2.0:cm:bearer\"><SubjectConfirmationData InResponseTo=\"123\" NotOnOrAfter=\"2010-06-01T13:03:00Z\" Recipient=\"http://saml.acs.url\"></SubjectConfirmationData></SubjectConfirmation></Subject><Conditions NotBefore=\"2010-06-01T12:59:55Z\" NotOnOrAfter=\"2010-06-01T16:00:00Z\"><AudienceRestriction><Audience>http://example.com</Audience></AudienceRestriction></Conditions><AuthnStatement AuthnInstant=\"2010-06-01T13:00:00Z\" SessionIndex=\"_abc\"><AuthnContext><AuthnContextClassRef>urn:oasis:names:tc:SAML:2.0:ac:classes:Password</AuthnContextClassRef></AuthnContext></AuthnStatement></Assertion>")
+        end
+      end
+
+      context 'setting a name_id_format on the principal' do
+        let(:name_id_format) { {} }
+        let(:principal) { instance_double('Principal') }
+
+        before do
+          expect(name_id_format).to receive(:fetch).with(:name).once.and_call_original
+          expect(name_id_format).to receive(:fetch).with(:getter).once.and_call_original
+          expect(name_id_format).not_to receive(:[])
+
+          expect(principal).to receive(:name_id_format).and_return(name_id_format).once
+        end
+
+        it 'uses an email name_id_format from the principal' do
+          expected_name_id = '<NameID Format="urn:oasis:names:tc:SAML:1.1:nameid-format:emailAddress">test@example.com</NameID>'
+
+          name_id_format[:name] = Saml::XML::Namespaces::Formats::NameId::EMAIL_ADDRESS
+          name_id_format[:getter] = proc {|p| p.email }
+
+          expect(principal).to receive(:email).once.and_return('test@example.com')
+
+          builder = described_class.new(
+            reference_id,
+            issuer_uri,
+            principal,
+            audience_uri,
+            saml_request_id,
+            saml_acs_url,
+            algorithm,
+            authn_context_classref,
+            expiry
+          )
+
+          expect(builder.send(:name_id_format)).to eq name_id_format
+
+          Timecop.travel(Time.zone.local(2010, 6, 1, 13, 0, 0)) do
+            raw = builder.raw
+
+            expect(raw).to include('test@example.com</NameID>')
+
+            expect(raw).to eq('<Assertion xmlns="urn:oasis:names:tc:SAML:2.0:assertion" ID="_abc" IssueInstant="2010-06-01T13:00:00Z" Version="2.0"><Issuer>http://sportngin.com</Issuer><Subject>' + expected_name_id + '<SubjectConfirmation Method="urn:oasis:names:tc:SAML:2.0:cm:bearer"><SubjectConfirmationData InResponseTo="123" NotOnOrAfter="2010-06-01T13:03:00Z" Recipient="http://saml.acs.url"></SubjectConfirmationData></SubjectConfirmation></Subject><Conditions NotBefore="2010-06-01T12:59:55Z" NotOnOrAfter="2010-06-01T16:00:00Z"><AudienceRestriction><Audience>http://example.com</Audience></AudienceRestriction></Conditions><AuthnStatement AuthnInstant="2010-06-01T13:00:00Z" SessionIndex="_abc"><AuthnContext><AuthnContextClassRef>urn:oasis:names:tc:SAML:2.0:ac:classes:Password</AuthnContextClassRef></AuthnContext></AuthnStatement></Assertion>')
+          end
+        end
+
+        it 'uses a persistent name_id_format from the principal' do
+          expected_name_id = '<NameID Format="urn:oasis:names:tc:SAML:2.0:nameid-format:persistent">some-fake-uuid</NameID>'
+
+          name_id_format[:name] = Saml::XML::Namespaces::Formats::NameId::PERSISTENT
+          name_id_format[:getter] = :get_uuid_function
+
+          expect(principal).to receive(:get_uuid_function).once.and_return('some-fake-uuid')
+
+          builder = described_class.new(
+            reference_id,
+            issuer_uri,
+            principal,
+            audience_uri,
+            saml_request_id,
+            saml_acs_url,
+            algorithm,
+            authn_context_classref,
+            expiry
+          )
+
+          expect(builder.send(:name_id_format)).to eq name_id_format
+
+          Timecop.travel(Time.zone.local(2010, 6, 1, 13, 0, 0)) do
+            raw = builder.raw
+
+            expect(raw).to include('some-fake-uuid</NameID>')
+
+            expect(raw).to eq('<Assertion xmlns="urn:oasis:names:tc:SAML:2.0:assertion" ID="_abc" IssueInstant="2010-06-01T13:00:00Z" Version="2.0"><Issuer>http://sportngin.com</Issuer><Subject>' + expected_name_id + '<SubjectConfirmation Method="urn:oasis:names:tc:SAML:2.0:cm:bearer"><SubjectConfirmationData InResponseTo="123" NotOnOrAfter="2010-06-01T13:03:00Z" Recipient="http://saml.acs.url"></SubjectConfirmationData></SubjectConfirmation></Subject><Conditions NotBefore="2010-06-01T12:59:55Z" NotOnOrAfter="2010-06-01T16:00:00Z"><AudienceRestriction><Audience>http://example.com</Audience></AudienceRestriction></Conditions><AuthnStatement AuthnInstant="2010-06-01T13:00:00Z" SessionIndex="_abc"><AuthnContext><AuthnContextClassRef>urn:oasis:names:tc:SAML:2.0:ac:classes:Password</AuthnContextClassRef></AuthnContext></AuthnStatement></Assertion>')
+          end
+        end
+
+      end
+    end
+
     it "builds encrypted XML" do
       builder = described_class.new(
         reference_id,
